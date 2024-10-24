e0c8b85b5dbb90e85f5bb1bf4a36b3c7c67c17cb
Syedaffanmubeen <syedaffanmubeen786@gmail.com>
2024-10-24 17:44:47 +0530

    add pattern.c

--- README.md
+++
@@ -1,2 +0,0 @@
-# C-Programs
-# C-Programs
---
+++ pattern.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+    int n,m;
+    char pattern = '$';
+    printf("Enter n : ");
+    scanf("%d",&n);
+    printf("Enter m : ");
+    scanf("%d",&m);
+    char arr[n];
+    int x=1;
+    if((n%2)!=0 && (m%2)!=0)
+    {
+        for(int i=0;x<=n;i++)
+        {
+            arr[i] = pattern;
+            if(x%m == 0)
+            {
+                if(pattern == '$')
+                {
+                    pattern = '*';
+                }
+                else
+                {
+                    pattern = '$';
+                }
+            }
+            x++;
+        }
+        arr[n]='\0';
+        printf("%s\n",arr);
+    }
+}
